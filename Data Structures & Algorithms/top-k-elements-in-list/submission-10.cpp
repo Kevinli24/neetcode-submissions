f1dced6909db3efe84e2daec6c5a29269c3abcc3
Kevin Li <110002169+Kevinli24@users.noreply.github.com>
2026-07-31 15:33:27 -0500

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqs;
+        vector<int> result;
+
+        for (const int& num : nums)
+        {
+            freqs[num]++;
+        }
+
+
+        vector<vector<int>> buckets(nums.size() + 1);
+
+        for (const auto& [num, frequency] : freqs)
+        {
+            buckets[frequency].push_back(num);
+        }
+
+        for (int frequency = nums.size(); frequency >= 1; frequency--)
+        {
+            for (int number : buckets[frequency])
+            {
+                result.push_back(number);
+
+                if (result.size() == k)
+                {
+                    return result;
+                }
+            }
+        }
+    }
+};
