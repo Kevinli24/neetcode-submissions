a8eaf8d5a3c0612b10c32f9bfd9369dd25c3b3e2
Kevin Li <110002169+Kevinli24@users.noreply.github.com>
2026-07-31 20:01:26 -0500

    Add: longest-consecutive-sequence - submission-19

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-19.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        /*
+        A sequence starts with the lowest value, then increments by 1.
+
+        First thoughts:
+        Find a number, if there's no number - 1, it's the start of a sequence
+        --> find min + 1, min + 2, etc... keeping track of length using a counter.
+
+        Then find other start numbers, and keep track of length.
+
+        Find max of all the counters.
+        */
+        int result = 0;
+
+        unordered_set<int> uniquenums;
+        for (int num : nums)
+        {
+            uniquenums.insert(num);
+        }
+
+        for (int j = 0; j < nums.size(); ++j)
+        {
+            int checkifstart = nums[j] - 1;
+            if (uniquenums.count(checkifstart) == 0) // Start of a sequence
+            {
+                int count = 1;
+                int newnum = nums[j];
+
+                while(uniquenums.count(newnum+1))
+                {
+                    ++count;
+                    ++newnum;
+                }
+                if (count > result)
+                {
+                        result = count;
+                }
+            }
+        }
+        return result;
+    }
+};
