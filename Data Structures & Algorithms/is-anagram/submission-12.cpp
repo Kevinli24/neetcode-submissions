6a6712d2f783ba5178185d0ec4a08c36b08b794a
Kevin Li <110002169+Kevinli24@users.noreply.github.com>
2026-08-02 00:38:58 -0500

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::unordered_map<char, int> mapfors;
+        std::unordered_map<char, int> mapfort;
+
+        for (const char& c : s)
+        {
+            mapfors[c]++;
+        }
+        for (const char& c : t)
+        {
+            mapfort[c]++;
+        }
+        if (mapfors == mapfort)
+        {
+            return true;
+        }
+        return false;
+
+
+        
+        
+    }
+};
